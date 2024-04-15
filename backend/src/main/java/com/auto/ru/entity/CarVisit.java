95c80d69dffa03acf82282eba74c4ddd65ea9de8
Daniil Gavrilov <dany.gavrik@mail.ru>
2024-04-15 22:42:04 +0700

    каскадное удаление работ в посещение

--- backend/src/main/java/com/auto/ru/entity/CarVisit.java
+++ backend/src/main/java/com/auto/ru/entity/CarVisit.java
@@ -1,5 +1,6 @@
 package com.auto.ru.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -35,7 +36,7 @@ public class CarVisit {
     @Column(name = "date_visit")
     private Instant visitDate = Instant.now();
 
-    @OneToMany
+    @OneToMany(cascade = CascadeType.REMOVE)
     @Column(name = "jobs")
     private List<Job> jobs;
 
