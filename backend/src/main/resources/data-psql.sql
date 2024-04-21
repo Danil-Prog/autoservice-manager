607de6f36dd40f60cf14ee55718df7f3e22e6a8a
Daniil Gavrilov <dany.gavrik@mail.ru>
2024-04-21 23:20:11 +0700

    - начальные данные

--- backend/src/main/resources/data-psql.sql
+++ backend/src/main/resources/data-psql.sql
@@ -1,2 +1,17 @@
+-- auto-generated definition
+create table IF NOT EXISTS autoservice_manager_user
+(
+    id       bigserial
+        primary key,
+    password varchar(255),
+    role     varchar(255)
+        constraint autoservice_manager_user_role_check
+            check ((role)::text = 'ROLE_ADMIN'::text),
+    username varchar(255)
+);
+
+alter table autoservice_manager_user
+    owner to "manager-user";
+
 INSERT INTO autoservice_manager_user (id, password, role, username)
 VALUES (1, '$2a$10$FLoqjXRFCplOEu3Jst3gt.Aco1ejRRBKFm9eeknPgpGGywctah9OK', 'ROLE_ADMIN', 'admin') ON CONFLICT DO NOTHING;
