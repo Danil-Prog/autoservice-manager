df11f384a39e324d4af616a70089717a2e415857
Daniil Gavrilov <dany.gavrik@mail.ru>
2024-05-05 22:41:15 +0700

    - поиск пользователя по кастоному запросу

--- backend/src/main/java/com/auto/ru/repository/UserRepository.java
+++ backend/src/main/java/com/auto/ru/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.auto.ru.repository;
 
 import com.auto.ru.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT user FROM User as user WHERE user.username = :username")
     Optional<User> findByUsername(String username);
 }
